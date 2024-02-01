d443f5831416b4212016634fe62203c5b3353b40
Tomas Gutierrez <tomasgutierrezdev@gmail.com>
2024-02-01 13:08:03 -0300

    Modify App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TodoList from './TodoList';
 
-function App() {
+const App: React.FC = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React', completed: false },
+    { id: 2, text: 'Build a React app', completed: false },
+  ]);
+
+  const [newTodoText, setNewTodoText] = useState('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTodoText(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (newTodoText.trim() !== '') {
+      setTodos((prevTodos) => [
+        ...prevTodos,
+        { id: prevTodos.length + 1, text: newTodoText, completed: false },
+      ]);
+
+      setNewTodoText('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Tomi todo list</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a new todo..."
+          value={newTodoText}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleAddTodo}>Add Todo</button>
+      </div>
+
+      <TodoList todos={todos} />
     </div>
   );
-}
+};
 
 export default App;
