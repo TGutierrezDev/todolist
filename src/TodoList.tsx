077c146b1b23b41e3d8706e867338d4c8ecad114
Tomas Gutierrez <tomasgutierrezdev@gmail.com>
2024-02-01 12:56:56 -0300

    Add base feature.

---
+++ src/TodoList.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+interface TodoListProps {
+  todos: Todo[];
+}
+
+const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+  const [checked, setChecked] = useState(false);
+	
+  const handleCheckboxChange = (todo: Todo) => {
+  	todo.completed = !todo.completed;
+  	setChecked((lastValue) => !lastValue);
+  }
+
+  return (
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>{todo.text}{        
+			<input
+				type="checkbox"
+				checked={todo.completed}
+				onChange={() => handleCheckboxChange(todo)}
+		  	/>}
+		  </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
